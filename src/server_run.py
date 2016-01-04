3376b82a396586458facc3b69efbe96325180fdb
Robert Speare <rspeare@il-pg-alpha-78567.use1.palantir.global>
2016-01-04 10:02:36 -0500

    pushing from server

--- src/server_run.py
+++ src/server_run.py
@@ -1,25 +1,37 @@
 import numpy as np
 import pandas as pd
 import mTSP
-
-NN=1477
-m=mTSP.mission(20)
-m.init_small_trips(NN)
+fname='../data/local_search_owl_70_1.2523_v2.csv'
+m=mTSP.mission(10)
+print('reading submission')
+m.read_submission(fname)
+print('checking loads and dictionary')
 m.check_loads()
 m.check_tmap()
 
-def anneal(numtrips,t,m3):
-    i=0
-    while(True):
-        var2=m.burn_swap(m3,t)
-        var=m.burn_merge(m3,t)
-        print('score: '+str(m.loss())+' var2: '+str(var2))
-        m.check_tmap()
-        m.check_loads()
-        if (var2<3.0):
-            t*=.99
-            print('Log(T)',np.log(t)/np.log(10))
-        if (i%10 ==0):
-            m.write_submission('../data/'+str(numtrips)+'trips.csv')
-        i+=1
+t=10**3
+m0=10**4
+i=0
+while(True):
+    i+=1
+    print('burning Kswaps...')
+    var=m.burn_Kswap(4,m0,t,m.gifts)
+    var=m.burn_Kswap(3,m0,t,m.gifts)
+    print('burning merges...')
+    m.burn_merge(m0,t)
+    print('burning splits...')
+    m.burn_split(m0,t)
+    print('reordering trips...')
+    m.reorder_all_trips()
+    print('checking things...')
+    m.check_tmap()
+    m.check_loads()
+    print('score : '+str(m.loss())+' temp: '+str(t)+' var '+str(var))
+    if (var < 3.0):
+        t*=0.99
+    if (i %10 ==0):
+        print('writing to file...')
+        m.write_submission(fname)
+        m.read_submission(fname)
+        print('number of trips ',len(m.trips))
 
